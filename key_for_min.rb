f1548ca2d092511ca751d3e3808bd6bb29d675a4
Melinda Diaz <melindadiaz@me.com>
2020-07-21 01:20:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ lowest_value = 0
 lowest_key= nil 
 
 name_hash.each do |key, value|
-  if lowest_value == 0 || value 
+  if lowest_value == 0 || value < lowest_value
+    value = lowest_value
   
 end
  
