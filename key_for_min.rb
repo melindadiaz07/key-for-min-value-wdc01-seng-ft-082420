72f61c0177fb9e01cc76cd18c8b5e44b10940bcd
Melinda Diaz <melindadiaz@me.com>
2020-07-21 01:18:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,9 @@
 
 def key_for_min_value(name_hash)
 
- name_hash.each do |key, value|
-   lowest_key = ""
- value_to_compare = name_hash[key]
- if name_hash[key] < value_to_compare
-   lowest_key = name_hash[key]
- end
-   lowest_key
-   
- end
+lowest_value = 0
+lowest_key= nil 
+
+
  
 end
